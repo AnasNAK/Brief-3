6807c73a7816f2d378e223ceaa1ade585d1586a8
AnasNAK <anasc00ding@gmail.com>
2022-11-16 17:18:24 +0100

    modification in function

--- style.js
+++ style.js
@@ -3,11 +3,11 @@ const burger = document.querySelector('.menu-icon')
 const links = document.querySelector('.nav-links')
 const close = document.querySelector('.close')
 
-burger.addEventListener('click' , function (){
+burger.addEventListener('click', function () {
     links.classList.add('show')
 })
 
-close.addEventListener('click' , function(){
+close.addEventListener('click', function () {
     links.classList.remove('show')
 })
 
@@ -20,32 +20,32 @@ let sushiClass = document.querySelector(".sushi");
 let ramenClass = document.querySelector(".ramen");
 let odenClass = document.querySelector(".oden");
 let btnall = document.getElementById('all');
-let btnSushi  = document.getElementById('Sushi');
+let btnSushi = document.getElementById('Sushi');
 let btnRamen = document.getElementById('Ramen');
 let btnOden = document.getElementById('Oden');
 
-btnall.addEventListener('click',function(){
-    sushiClass.style.display="flex";
-    ramenClass.style.display="flex";
-    odenClass.style.display="flex";
+btnall.addEventListener('click', function () {
+    sushiClass.style.display = "flex";
+    ramenClass.style.display = "flex";
+    odenClass.style.display = "flex";
 
 });
-btnSushi.addEventListener('click',function(){
-    sushiClass.style.display="flex";
-    ramenClass.style.display="none";
-    odenClass.style.display="none";
+btnSushi.addEventListener('click', function () {
+    sushiClass.style.display = "flex";
+    ramenClass.style.display = "none";
+    odenClass.style.display = "none";
 
 });
-btnOden.addEventListener('click',function(){
-    sushiClass.style.display="none";
-    ramenClass.style.display="none";
-    odenClass.style.display="flex";
+btnOden.addEventListener('click', function () {
+    sushiClass.style.display = "none";
+    ramenClass.style.display = "none";
+    odenClass.style.display = "flex";
 
 });
-btnRamen.addEventListener('click',function(){
-    sushiClass.style.display="none";
-    ramenClass.style.display="flex";
-    odenClass.style.display="none";
+btnRamen.addEventListener('click', function () {
+    sushiClass.style.display = "none";
+    ramenClass.style.display = "flex";
+    odenClass.style.display = "none";
 
 });
 
@@ -53,14 +53,14 @@ btnRamen.addEventListener('click',function(){
 
 //caart
 let arrTotal = [];
-var somme = 0 ;
+var somme = 0;
 let eventclickadd = document.querySelectorAll('.buy');
 let parentcart = document.querySelector('.p-cart');
 
 let idt = document.getElementById('idt');
 
-eventclickadd.forEach(function(element){
-    element.addEventListener('click' ,function(e) {
+eventclickadd.forEach(function (element) {
+    element.addEventListener('click', function (e) {
         let obj = e.target.parentNode;
         console.log(obj.childNodes);
         let titreObj = obj.childNodes[1].innerText;
@@ -69,17 +69,28 @@ eventclickadd.forEach(function(element){
         console.log(prixObj);
         let total = document.getElementById('price');
         let div = document.createElement("div");
-        div.innerHTML=`<div class ="ur-cart">
+        div.innerHTML = `<div class ="ur-cart">
        <div class ="name-Obj">${titreObj}</div>
-       <div class ="price-Obj">${prixObj }</div>
+       <div class ="price-Obj">${prixObj}</div>
         </div>`;
         idt.appendChild(div);
         arrTotal.push(parseInt(prixObj));
-        somme = arrTotal.reduce((p, c)=>p+c, 0) ;
-        total.innerHTML=somme; 
+        somme = arrTotal.reduce((p, c) => p + c, 0);
+        total.innerHTML = somme;
     });
 
 });
+// // remove items from carte
+// function removeCarteItem(event) {
+//     var buttonClicked = event.target;
+//     buttonClicked.parentElement.remove(".ur-cart");
+// }
 
-
-
+// function ready() {
+//     // --- effacer les items de carte ---
+//     var removeCarteButtons = document.getElementsByClassName("msa7");
+//     for (var i = 0; i < removeCarteButtons.length; i++) {
+//       var button = removeCarteButtons[i];
+//       button.addEventListener("click", removeCarteItem);
+//     }
+// }
